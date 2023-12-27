0f591a7acbd7e8e2d8565912aa06be9a9cbc8a9e
moinbukhari <moinbukhari4321@gmail.com>
2023-12-27 20:02:31 +0000

    moved skills to bottom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -159,15 +159,6 @@ export default function Page() {
           })}
         </Section>
 
-        <Section>
-          <h2 className="text-xl font-bold">Skills</h2>
-          <div className="flex flex-wrap gap-1">
-            {RESUME_DATA.skills.map((skill) => {
-              return <Badge key={skill}>{skill}</Badge>;
-            })}
-          </div>
-        </Section>
-
         <Section className="print-force-new-page scroll-mb-16">
           <h2 className="text-xl font-bold">Projects</h2>
           <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
@@ -181,6 +172,14 @@ export default function Page() {
                   link={"link" in project ? project.link.href : undefined}
                 />
               );
+            })}
+          </div>
+        </Section>
+        <Section>
+          <h2 className="text-xl font-bold">Skills</h2>
+          <div className="flex flex-wrap gap-1">
+            {RESUME_DATA.skills.map((skill) => {
+              return <Badge key={skill}>{skill}</Badge>;
             })}
           </div>
         </Section>
